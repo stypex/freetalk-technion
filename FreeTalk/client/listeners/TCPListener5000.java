524e626d094454dd863035b6a0d4b47228ec6ca8
stypex <stypex@ab65d0b8-1240-0410-a2dd-27fd2c32d56d>
2007-12-09 19:01:12 +0000

    TCPListener5000 added

--- FreeTalk/client/listeners/TCPListener5000.java
+++ FreeTalk/client/listeners/TCPListener5000.java
@@ -3,10 +3,58 @@
  */
 package client.listeners;
 
+import interfaces.TCPIncomingInterface;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import messages.Message;
+import java.util.List;
+import client.data.ClientsList;
+import client.data.ConferenceCallsHash;
+import client.func.TalkThread;
+import client.listeners.StoppableThread;
+
+
 /**
- * @author lenka
+ * @author Ilya
  *
  */
 public class TCPListener5000 extends StoppableThread {
 
+	ServerSocket ss;
+	
+	public TCPListener5000() {
+		super();
+		try {
+			ss = new ServerSocket(5000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void run()  {
+		while (isStopped == false) {
+			try {
+				Socket s = ss.accept();
+				TCPIncomingInterface in = new TCPIncomingInterface(s);
+				Message m = in.receive(0);
+				
+				String from = m.getFrom();
+				String to = m.getTo();
+				
+				System.out.println("from=");
+				System.out.println(from);
+				System.out.println("to=");
+				System.out.println(to);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 }
