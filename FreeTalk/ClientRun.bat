a01a3e6b45eea7cb231aee71d60128d903522aae
arthur.sandflee <arthur.sandflee@ab65d0b8-1240-0410-a2dd-27fd2c32d56d>
2007-12-31 12:53:48 +0000

    Added batch files for running the project outside of eclipse and decreased the exp backoff until 4 sec to save debug time

---
+++ FreeTalk/ClientRun.bat
@@ -0,0 +1,16 @@
+@echo off
+echo *********************************************************************
+echo *				FreeTalk				 
+echo *	By :
+echo *		Arthur Kiyanovsky, Lena Zarkhin, Ilya Bokshtein
+echo *	Supervisor :
+echo *		Itai Dabran
+echo *	Lab :
+echo *		CS Networks at the Technion IIT
+echo *	Semester :
+echo *		Winter 2007-2008
+echo *********************************************************************
+
+echo Running Client..
+java client.ClientMain
+
---
+++ FreeTalk/ServerRun.bat
@@ -0,0 +1,17 @@
+@echo off
+echo *********************************************************************
+echo *				FreeTalk				 
+echo *	By :
+echo *		Arthur Kiyanovsky, Lena Zarkhin, Ilya Bokshtein
+echo *	Supervisor :
+echo *		Itai Dabran
+echo *	Lab :
+echo *		CS Networks at the Technion IIT
+echo *	Semester :
+echo *		Winter 2007-2008
+echo *********************************************************************
+
+echo Running Server..
+java -server server.listeners.ServerMainListener
+pause
+
