243b1856d2a2f6de46401802243a61deb4e5bd6a
stypex <stypex@ab65d0b8-1240-0410-a2dd-27fd2c32d56d>
2008-01-03 15:55:59 +0000

    added syncs in Chat

--- FreeTalk/client/gui/Chat.java
+++ FreeTalk/client/gui/Chat.java
@@ -37,7 +37,7 @@ public class Chat extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private TalkThread tt;
-	
+
 	private String userName;
 	private JTextPane utp;
 	private JTextPane ltp;
@@ -62,11 +62,11 @@ public class Chat extends JFrame {
 	 * chat window.
 	 */
 	public Chat(String destUserName, TalkThread tt){
-		
+
 		setIconImage(Toolkit.getDefaultToolkit().getImage("Images\\chat-48x48.png"));
-		
+
 		this.tt = tt;
-		
+
 		this.userName = Globals.getClientName();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setTitle(destUserName);
@@ -75,14 +75,14 @@ public class Chat extends JFrame {
 		statusP.setLayout(new BorderLayout());
 		statusL = new JLabel();
 		statusP.add(statusL, BorderLayout.WEST);
-		
+
 		//Text areas initializations
 		utp = new JTextPane();
 		utp.setEditable(false);
 		ltp = new JTextPane();
 		usp = new JScrollPane(utp);
 		lsp = new JScrollPane(ltp);
-		
+
 		//Create list of users in chat
 		lstModel = new DefaultListModel();
 		lstModel.addElement(destUserName);
@@ -90,7 +90,7 @@ public class Chat extends JFrame {
 		lst.setSelectionBackground(Color.white);
 		lst.setFocusable(false); //to avoid selecting items in the list
 		rsp = new JScrollPane(lst);
-		
+
 		//Create ComboBox of users that can be added to chat
 		cbModel = new DefaultComboBoxModel();
 		cbModel.addElement("Add user to chat");
@@ -98,7 +98,7 @@ public class Chat extends JFrame {
 			if (!u.equals(destUserName))
 				cbModel.addElement((String)u);
 		addToChat = new JComboBox (cbModel);
-		
+
 		//Attach the ComboBox selection to adding the user to the chat
 		addToChat.addActionListener(new ActionListener() {
 
@@ -107,13 +107,13 @@ public class Chat extends JFrame {
 				if (!client.equals("Add user to chat"))
 					addClientToSession(client);
 			}
-			
+
 		});
-		
-		
+
+
 		send = new JButton("Send");
 		send.setEnabled(false);
-		
+
 		//This part doesn't allow sending text that is only whitespaces
 		//by disabling the "Send" button.
 		ltp.getDocument().addDocumentListener(new DocumentListener() {
@@ -129,85 +129,85 @@ public class Chat extends JFrame {
 			public void removeUpdate(DocumentEvent arg0) {
 				checkTextEntered();
 			}
-        });
-		
+		});
+
 		//Attaches between pressing the "Send" button and the message being sent
 		//as well as shown in the chat text area
 		send.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	send();
-            	moveTextToChatWindow();
-            }
-        });
-		
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				send();
+				moveTextToChatWindow();
+			}
+		});
+
 		p = new JPanel();
 		//Set the layout of the right side of the window
 		GroupLayout pLayout = new GroupLayout(p);
-	    p.setLayout(pLayout);
-	    pLayout.setHorizontalGroup(
-	    		pLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-	    	.addGroup(pLayout.createSequentialGroup()
-	    	    .addComponent(addToChat, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-	    	.addGroup(pLayout.createSequentialGroup()
-	        	.addComponent(rsp, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-	        .addGroup(pLayout.createSequentialGroup()
-	    	    .addComponent(send, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-	        	
-	    );
-	    pLayout.setVerticalGroup(
-	    		pLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-	        .addGroup(pLayout.createSequentialGroup()
-	        	.addComponent(addToChat, 25, 25, 25)
-	        	.addComponent(rsp, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
-		        .addComponent(send, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))       
-	    );
-		
-		
-	    usp.setMinimumSize(new Dimension(100,25));
+		p.setLayout(pLayout);
+		pLayout.setHorizontalGroup(
+				pLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+				.addGroup(pLayout.createSequentialGroup()
+						.addComponent(addToChat, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
+						.addGroup(pLayout.createSequentialGroup()
+								.addComponent(rsp, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
+								.addGroup(pLayout.createSequentialGroup()
+										.addComponent(send, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
+
+		);
+		pLayout.setVerticalGroup(
+				pLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+				.addGroup(pLayout.createSequentialGroup()
+						.addComponent(addToChat, 25, 25, 25)
+						.addComponent(rsp, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+						.addComponent(send, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))       
+		);
+
+
+		usp.setMinimumSize(new Dimension(100,25));
 		lsp.setMinimumSize(new Dimension(100,25));
-		
+
 		//Group components for easier setup of the layout
 		sp1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,usp,lsp);
 		sp1.setResizeWeight(0.8);
 
 		sp2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,sp1,p);
 		sp2.setResizeWeight(1);
-		
+
 		//Set the layout of the window
 		GroupLayout chatLayout = new GroupLayout(getContentPane());
-	    getContentPane().setLayout(chatLayout);
-	    chatLayout.setHorizontalGroup(
-	    		chatLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-	        .addGroup(chatLayout.createSequentialGroup()
-	        	.addGap(10)
-	        	.addComponent(sp2, GroupLayout.DEFAULT_SIZE, 450, GroupLayout.DEFAULT_SIZE)
-	        	.addGap(10))
-	        .addGroup(chatLayout.createSequentialGroup()
-	        	.addComponent(statusP, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
-	    );
-	    chatLayout.setVerticalGroup(
-	    		chatLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-	        .addGroup(chatLayout.createSequentialGroup()
-	        	.addGap(10)
-		        .addComponent(sp2, GroupLayout.DEFAULT_SIZE, 300, GroupLayout.DEFAULT_SIZE)
-	        	.addGap(10)
-	        	.addComponent(statusP, 20, 20, 20))
-	    );
-		
+		getContentPane().setLayout(chatLayout);
+		chatLayout.setHorizontalGroup(
+				chatLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+				.addGroup(chatLayout.createSequentialGroup()
+						.addGap(10)
+						.addComponent(sp2, GroupLayout.DEFAULT_SIZE, 450, GroupLayout.DEFAULT_SIZE)
+						.addGap(10))
+						.addGroup(chatLayout.createSequentialGroup()
+								.addComponent(statusP, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE))
+		);
+		chatLayout.setVerticalGroup(
+				chatLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+				.addGroup(chatLayout.createSequentialGroup()
+						.addGap(10)
+						.addComponent(sp2, GroupLayout.DEFAULT_SIZE, 300, GroupLayout.DEFAULT_SIZE)
+						.addGap(10)
+						.addComponent(statusP, 20, 20, 20))
+		);
+
 		pack();
-		
+
 		//set the minimum dimensions of the different components in the window
 		send.setMinimumSize(new Dimension(p.getWidth(), 20));
 		p.setMinimumSize(new Dimension(p.getWidth(), addToChat.getHeight()*2 + send.getHeight()));
 		setMinimumSize(new Dimension( 50 + p.getWidth() + usp.getMinimumSize().width, 80 + addToChat.getHeight()*2 + statusP.getHeight()));
-		
+
 		//Set window position
-	    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation( ( d.width - getSize().width ) / 3, ( d.height - getSize().height ) / 3);
-		
+
 		ltp.requestFocusInWindow();
 	}
-	
+
 	/**
 	 * Checks if the text entered is only white spaces.
 	 * If so disables the send button.
@@ -220,7 +220,7 @@ public class Chat extends JFrame {
 		else
 			send.setEnabled(false);
 	}
-	
+
 	/**
 	 * delegates to TalkThread.send()
 	 * @author Arthur Kiyanovsky
@@ -229,7 +229,7 @@ public class Chat extends JFrame {
 	private void send(){
 		tt.send(ltp.getText());
 	}
-	
+
 	/**
 	 * Copies the text from where the user entered it to the chat 
 	 * text area in the correct format. 
@@ -238,11 +238,11 @@ public class Chat extends JFrame {
 	 */
 	private void moveTextToChatWindow() { 		
 		putTextInChatWindow(ltp.getText(), userName);
-		
+
 		ltp.setText(null);
 		ltp.requestFocusInWindow();
-	 }
-	
+	}
+
 	/**
 	 * Adds text to the chat window
 	 * @param text
@@ -252,7 +252,7 @@ public class Chat extends JFrame {
 		StyleContext sc = new StyleContext();
 		NamedStyle s = sc.new NamedStyle();
 		StyleConstants.setBold(s, true);
-		
+
 		try {
 			d.insertString(d.getLength(), from + " <" + util.Func.getDateTime() + ">\n", s);
 			StyleConstants.setBold(s, false);
@@ -262,7 +262,7 @@ public class Chat extends JFrame {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Removes the selected client from the "Add client to conference 
 	 * call" ComboBox and adds it to the conference list of clients. 
@@ -270,21 +270,21 @@ public class Chat extends JFrame {
 	 * Nov 30, 2007
 	 */
 	public void moveFromComboToList(String client){
-//		synchronized (cbModel){
-//			synchronized (lstModel) {
+		synchronized (cbModel){
+			synchronized (lstModel) {
 				cbModel.removeElement(client);
-				
+
 				Func.addAlphabeticallyToLM(client,lstModel);
-				
+
 				addToChat.setSelectedIndex(0);
-				
+
 				setTitle();
-//			}
-//		}
+			}
+		}
 	}
 
-	
-	
+
+
 	/**
 	 * Removes a given client from the conference list of clients
 	 * and adds it back to the "Add client to conference call" ComboBox. 
@@ -293,13 +293,16 @@ public class Chat extends JFrame {
 	 * Nov 30, 2007
 	 */
 	private void moveFromListToCombo(String client){
-		lstModel.removeElement(client);
-		
-		Func.addAlphabeticallyToCBM(client,cbModel);
+		synchronized (cbModel){
+			synchronized (lstModel) {
+				lstModel.removeElement(client);
 
+				Func.addAlphabeticallyToCBM(client,cbModel);
+			}
+		}
 		setTitle();
 	}
-	
+
 	/**
 	 * Adds a client to the "clients that can be added" ComboBox.
 	 * @param client - Client to be added.
@@ -307,9 +310,11 @@ public class Chat extends JFrame {
 	 * Dec 8, 2007
 	 */
 	public void addClient(String client){
-		Func.addAlphabeticallyToCBM(client,cbModel);
+		synchronized (cbModel){
+			Func.addAlphabeticallyToCBM(client,cbModel);
+		}
 	}
-	
+
 	/**
 	 * Adds a client to the conference call list of clients
 	 * and removes it from the "clients that can be added" ComboBox.
@@ -321,7 +326,7 @@ public class Chat extends JFrame {
 		tt.addclientToSession(client);
 		moveFromComboToList(client);
 	}
-	
+
 	/**
 	 * Removes a client from both "chat list" and "clients 
 	 * that can be added" ComboBox. Use this method when client
@@ -331,8 +336,12 @@ public class Chat extends JFrame {
 	 * Dec 8, 2007
 	 */
 	public void removeClient(String client){
-		cbModel.removeElement(client);
-		lstModel.removeElement(client);
+		synchronized (cbModel){
+			synchronized (lstModel) {
+				cbModel.removeElement(client);
+				lstModel.removeElement(client);
+			}
+		}
 		setTitle();
 		/* in case the last client in the conference disconnected no need to stay in
 		 * the receiveMessages() while loop that eats up CPU
@@ -340,7 +349,7 @@ public class Chat extends JFrame {
 		/*if ( lstModel.size() == 0 )
 			tt.doStop();*/
 	}
-	
+
 	/**
 	 * Removes a client from "chat list" and puts it back to
 	 * "clients that can be added" ComboBox  when the client
@@ -351,21 +360,23 @@ public class Chat extends JFrame {
 	 */
 	public void removeClientFromSession(String client){
 		moveFromListToCombo(client);
-		
+
 		setTitle();
 	}
-	
+
 	public void setStatusBarText(String s){
 		statusL.setText(" " + s);
 	}
-	
+
 	private void setTitle() {
-		
-		if (lstModel.getSize() >= 2)
-			setTitle("Conference");
-		else if (lstModel.getSize() == 1)
-			setTitle(lstModel.get(0).toString());
-		else 
-			setTitle("Nobody");
+
+		synchronized (lstModel) {
+			if (lstModel.getSize() >= 2)
+				setTitle("Conference");
+			else if (lstModel.getSize() == 1)
+				setTitle(lstModel.get(0).toString());
+			else 
+				setTitle("Nobody");
+		}
 	}
 }
